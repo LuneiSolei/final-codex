11a22a3c3ae690cc57b7c7e43233dfb1edcfd1f0
lunei-solei <68037318+LuneiSolei@users.noreply.github.com>
2026-03-27 03:53:58 +0000

    chore: add unit test for shared library configuration

---
+++ XivApiTests/UnitTest1.cs
@@ -0,0 +1,32 @@
+﻿using FinalCodex.SharedLibrary.Options;
+using FinalCodex.SharedLibrary.Services;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Options;
+
+namespace XivApiTests;
+
+public class Tests
+{
+    [Test]
+    public void BindOptionsTest()
+    {
+        // Mimic a project's local config that overrides appsettings.json
+        Dictionary<string, string?> testConfig = new()
+        {
+            ["SharedLibraryOptions:XivApiOptions:BaseApiUrl"] = "https://google.com"
+        };
+        IConfiguration config = new ConfigurationBuilder()
+            .AddInMemoryCollection(testConfig).Build();
+
+        IServiceCollection services = new ServiceCollection();
+        services.AddSharedLibraryService(config);
+
+        ServiceProvider provider = services.BuildServiceProvider();
+        SharedLibraryOptions sharedLibraryOptions = provider
+            .GetRequiredService<IOptions<SharedLibraryOptions>>().Value;
+        
+        Assert.That(sharedLibraryOptions.XivApiOptions.BaseApiUrl, 
+            Is.EqualTo("https://google.com"));
+    }
+}
