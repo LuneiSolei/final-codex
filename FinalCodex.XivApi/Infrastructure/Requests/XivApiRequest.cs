b87e6788a994c7d0bba59ed2e85acda5fb87c939
lunei-solei <68037318+LuneiSolei@users.noreply.github.com>
2026-04-07 21:03:58 +0000

    chore: add Schema and Options to XivApiRequest.cs

--- FinalCodex.XivApi/Infrastructure/Requests/XivApiRequest.cs
+++ FinalCodex.XivApi/Infrastructure/Requests/XivApiRequest.cs
@@ -7,12 +7,15 @@ namespace FinalCodex.XivApi.Infrastructure.Requests;
 public abstract class XivApiRequest
 {
     protected string BaseUrl;
+    protected XivApiOptions Options;
     protected string? Version;
     protected SchemaLanguage? Language;
+    protected string? Schema;
     protected List<Clause> QueryClauses = [];
     
     internal XivApiRequest(XivApiOptions opts)
     {
+        Options = opts;
         BaseUrl = $"{opts.Scheme}://{opts.BaseUrl}/";
     }
 }
