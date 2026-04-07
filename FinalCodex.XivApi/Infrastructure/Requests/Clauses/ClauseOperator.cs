3fadb2ca570e57f6c4eb167a4fff8e51589b4da5
lunei-solei <68037318+LuneiSolei@users.noreply.github.com>
2026-04-07 02:09:20 +0000

    chore: remove unused files

--- FinalCodex.XivApi/Infrastructure/Requests/Clauses/ClauseOperator.cs
+++
@@ -1,9 +0,0 @@
-// namespace FinalCodex.XivApi.Infrastructure.Request.Filter;
-//
-// public sealed class FilterOperator(string fieldName)
-// {
-//     public SubQueryParameter EqualTo(string value)
-//     {
-//         return new SubQueryParameter(fieldName, "=", value);
-//     }
-// }
--- FinalCodex.XivApi/Infrastructure/Requests/Clauses/Steps/IWhereFieldStep.cs
+++
@@ -1,6 +0,0 @@
-namespace FinalCodex.XivApi.Infrastructure.Requests.Clauses.Steps;
-
-public interface IWhereFieldStep
-{
-    IConditionStep WhereField(string fieldName);
-}
