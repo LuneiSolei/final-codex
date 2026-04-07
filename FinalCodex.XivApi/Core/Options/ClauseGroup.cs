06ba7221c6c5cad4acb2cf437c1457d99b2d1111
lunei-solei <68037318+LuneiSolei@users.noreply.github.com>
2026-04-07 21:16:14 +0000

    chore: begin implementing ClauseGroup.cs

--- FinalCodex.XivApi/Core/Options/ClauseGroup.cs
+++ FinalCodex.XivApi/Core/Options/ClauseGroup.cs
@@ -2,5 +2,11 @@ namespace FinalCodex.XivApi.Core.Options;
 
 public class ClauseGroup
 {
-    
+    public List<Clause> Clauses { get; } = [];
+    public string Operator = string.Empty;
+
+    public override string ToString()
+    {
+        return $"{Operator}({string.Join(' ', Clauses)})";
+    }
 }
