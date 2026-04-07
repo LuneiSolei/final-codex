a956a3a8b865f8d94242f200965dbfbf3fdbbc95
lunei-solei <68037318+LuneiSolei@users.noreply.github.com>
2026-04-07 21:31:45 +0000

    feat: implement Operator in ClauseGroup.cs

--- FinalCodex.XivApi/Core/Options/ClauseGroup.cs
+++ FinalCodex.XivApi/Core/Options/ClauseGroup.cs
@@ -1,12 +1,30 @@
+using FinalCodex.XivApi.Core.Enums;
+
 namespace FinalCodex.XivApi.Core.Options;
 
 public class ClauseGroup
 {
     public List<Clause> Clauses { get; } = [];
-    public string Operator = string.Empty;
-
+    public ClauseGroupOperator Operator { get; }
+    
+    public ClauseGroup(ClauseGroupOperator? groupOperator)
+    {
+        Operator = groupOperator ?? ClauseGroupOperator.Or;
+    }
+    
     public override string ToString()
     {
-        return $"{Operator}({string.Join(' ', Clauses)})";
+        return $"{GenerateOperator()}({string.Join(' ', Clauses)})";
+    }
+
+    private string GenerateOperator()
+    {
+        return Operator switch
+        {
+            ClauseGroupOperator.Or => string.Empty,
+            ClauseGroupOperator.Must => "+",
+            ClauseGroupOperator.MustNot => "-",
+            _ => string.Empty
+        };
     }
 }
